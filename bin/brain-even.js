b53a4d5821ccc590b96eccd1789b942b922ca8cc
Вадим <sryhanzom4in@yandex.ru>
2024-01-29 14:17:26 +0700

    surecommit

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -13,16 +13,16 @@ const isValEven = (answer) => {
     console.log('Correct!');
     count += 1;
   } else if (randomInt % 2 !== 0 && answer === 'yes') {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let\'s try again, ${name}!`);
+    console.log('"yes" is wrong answer ;(. Correct answer was "no".');
+    console.log(`Let's try again, ${name}!`);
     count += 10;
   } else if (randomInt % 2 === 0 && answer === 'no') {
-    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-    console.log(`Let\'s try again, ${name}!`);
+    console.log('"no" is wrong answer ;(. Correct answer was "yes".');
+    console.log(`Let's try again, ${name}!`);
     count += 10;
   }
 };
-console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 while (count < 3) {
   console.log(`Question: ${randomInt}`);
   const answer = readlineSync.question('Your answer: ');
