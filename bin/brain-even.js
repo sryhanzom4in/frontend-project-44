7f284a2ce2dbd635ac106f7ed847bc783e0210f9
Вадим <sryhanzom4in@yandex.ru>
2024-01-29 10:36:29 +0700

    brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,27 +1,34 @@
-import readlineSync, { question } from "readline-sync";
+import readlineSync from "readline-sync";
 import greetings from "../src/cli.js";
 
 const getRandomInt = (max = 1000) => Math.floor(Math.random() * max);
 let randomInt = getRandomInt();
 const name = greetings();
+let count = 0;
 const isValEven = (answer) => {
   if (randomInt % 2 === 0 && answer === "yes") {
     console.log("Correct!");
+    count += 1;
   } else if (randomInt % 2 !== 0 && answer === "no") {
     console.log("Correct!");
+    count += 1;
   } else if (randomInt % 2 !== 0 && answer === "yes") {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
     console.log(`Let\'s try again, ${name}!`);
+    count += 10;
   } else if (randomInt % 2 === 0 && answer === "no") {
     console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
     console.log(`Let\'s try again, ${name}!`);
+    count += 10;
   }
 };
 console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
-let count = 0;
 while (count < 3) {
   console.log(`Question: ${randomInt}`);
+  const answer = readlineSync.question("Your answer: ");
+  isValEven(answer);
+  randomInt = getRandomInt();
+  if (count === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
 }
-randomInt = getRandomInt();
-// const answer = readlineSync.question("Your answer: ");
-// isValEven(answer);
